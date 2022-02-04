77d743721c79db7a2c4982460d0706ff2e7b71d1
Abdul Rafey <csyedabdulrafeyali@gmail.com>
2022-02-05 00:27:16 +0500

    Main__NodeRestApi__Login&RegisterSystem

--- Node-Rest-API/routes/auth.js
+++ Node-Rest-API/routes/auth.js
@@ -1,8 +1,46 @@
 const router = require("express").Router();
+const User = require("../models/User")
+const bycrypt = require("bcrypt")
 
-// 
-router.get("/", (req, res) => {
-    res.send("Hey its auth route")
+
+// REGISTER
+router.post("/register", async (req, res) => {
+
+    try {
+        // Generating New Password
+        const salt = await bycrypt.genSalt(10);
+        const hashedPassword = await bycrypt.hash(req.body.password, salt)
+
+        // Create New User
+        const newUser = new User({
+            username: req.body.username,
+            email: req.body.email,
+            password: hashedPassword
+        });
+
+        // Save User & Return Response
+        const user = await newUser.save();
+        res.status(200).json(user)
+    } catch (err) {
+        res.status(500).json(err)
+    }
+})
+
+// LOGIN
+router.post("/login", async (req, res) => {
+    try {
+        const user = await User.findOne({ email: req.body.email })
+        !user && res.status(404).send("User Not found")
+
+        // Bcrypting Back
+        const validPassword = await bycrypt.compare(req.body.password, user.password)
+        !validPassword && res.status(400).json("wrong password")
+    
+        res.status(200).json(user);
+    }
+    catch(err){
+        res.status(500).json(err)
+    }
 })
 
 module.exports = router
