40788901a8a86a1da3752fb089ffa548a0c3b704
Abdul Rafey <csyedabdulrafeyali@gmail.com>
2022-02-05 20:57:55 +0500

    Main__NodeRestApi__Posts__CRUD

--- Node-Rest-API/routes/posts.js
+++ Node-Rest-API/routes/posts.js
@@ -1,7 +1,45 @@
 const router = require("express").Router()
-
+const Post = require("../models/Post")
 // create a post
-// create a post
-// create a post
+router.post("/", async (req, res) => {
+    const newPost = new Post(req.body);
+    try {
+        const savedPost = await newPost.save()
+        res.status(200).json(savedPost)
+    } catch (err) {
+        res.status(500).json(err);
+    }
+})
+// update a post
+router.put("/:id", async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+        if (post.userId === req.body.userId) {
+            await post.updateOne({$set: req.body})
+            res.status(200).json("The Post has been updated")
+        } else {
+            req.status(403).json("You can update only your post")
+        }
+    } catch(err){
+        res.status(500).json(err)
+    }
+})
+// delete a post
+router.delete("/:id", async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+        if (post.userId === req.body.userId) {
+            await post.deleteOne()
+            res.status(200).json("The Post has been deleted")
+        } else {
+            req.status(403).json("You can delete only your post")
+        }
+    } catch(err){
+        res.status(500).json(err)
+    }
+})
+// like a post
+// get a post
+// get timeline posts
 
 module.exports = router;
