bb6c0a529239221012d845711e1531720d252db0
Abdul Rafey <csyedabdulrafeyali@gmail.com>
2022-02-05 22:34:44 +0500

    Main__ReactJS__Installation

---
+++ react-frontend/src/App.js
@@ -0,0 +1,12 @@
+import Home from "./pages/home/Home";
+import { Person } from "@material-ui/icons"
+
+function App() {
+  return (<>
+    <Home />
+    <Person />
+  </>
+  );
+}
+
+export default App;
