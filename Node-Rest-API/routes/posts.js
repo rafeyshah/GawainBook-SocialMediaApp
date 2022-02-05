cd8692ac1452a0dbf7e936b3a65dcd135509086c
Abdul Rafey <csyedabdulrafeyali@gmail.com>
2022-02-05 21:21:13 +0500

    Main__NodeRestApi__LikeDislikePost

--- Node-Rest-API/routes/posts.js
+++ Node-Rest-API/routes/posts.js
@@ -15,12 +15,12 @@ router.put("/:id", async (req, res) => {
     try {
         const post = await Post.findById(req.params.id);
         if (post.userId === req.body.userId) {
-            await post.updateOne({$set: req.body})
+            await post.updateOne({ $set: req.body })
             res.status(200).json("The Post has been updated")
         } else {
             req.status(403).json("You can update only your post")
         }
-    } catch(err){
+    } catch (err) {
         res.status(500).json(err)
     }
 })
@@ -34,12 +34,34 @@ router.delete("/:id", async (req, res) => {
         } else {
             req.status(403).json("You can delete only your post")
         }
+    } catch (err) {
+        res.status(500).json(err)
+    }
+});
+// like a post
+router.put("/:id/like", async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+        if (!post.likes.includes(req.body.userId)) {
+            await post.updateOne({ $push: { likes: req.body.userId } });
+            res.status(200).json("The post has been liked")
+        } else{
+            await post.updateOne({$pull: {likes: req.body.userId}})
+            res.status(200).json("The post has been disliked")
+        }
+    } catch (err) {
+        res.status(500).json(err)
+    }
+})
+// get a post
+router.get("/:id", async(req, res)=>{
+    try{
+        const post = await Post.findById(req.params.id);
+        res.status(200).json(post);
     } catch(err){
         res.status(500).json(err)
     }
 })
-// like a post
-// get a post
 // get timeline posts
 
 module.exports = router;
