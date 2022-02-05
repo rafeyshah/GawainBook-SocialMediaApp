80477d5d3b966cc726107d96139a93a27ae1e64c
Abdul Rafey <csyedabdulrafeyali@gmail.com>
2022-02-05 22:04:29 +0500

    Main__NodeRestApi__GetTimeLinePost__NestedAsynchoronusFetch

--- Node-Rest-API/routes/posts.js
+++ Node-Rest-API/routes/posts.js
@@ -1,5 +1,6 @@
 const router = require("express").Router()
 const Post = require("../models/Post")
+const User = require("../models/User")
 // create a post
 router.post("/", async (req, res) => {
     const newPost = new Post(req.body);
@@ -45,8 +46,8 @@ router.put("/:id/like", async (req, res) => {
         if (!post.likes.includes(req.body.userId)) {
             await post.updateOne({ $push: { likes: req.body.userId } });
             res.status(200).json("The post has been liked")
-        } else{
-            await post.updateOne({$pull: {likes: req.body.userId}})
+        } else {
+            await post.updateOne({ $pull: { likes: req.body.userId } })
             res.status(200).json("The post has been disliked")
         }
     } catch (err) {
@@ -54,14 +55,28 @@ router.put("/:id/like", async (req, res) => {
     }
 })
 // get a post
-router.get("/:id", async(req, res)=>{
-    try{
+router.get("/:id", async (req, res) => {
+    try {
         const post = await Post.findById(req.params.id);
         res.status(200).json(post);
-    } catch(err){
+    } catch (err) {
         res.status(500).json(err)
     }
 })
 // get timeline posts
+router.get("/timeline/all", async (req, res) => {
+    try {
+        const currentUser = await User.findById(req.body.userId);
+        const userPosts = await Post.find({ userId: currentUser._id });
+        // without promise, when fetching posts it's not gonna load all posts
+        const friendPosts = await Promise.all(
+            currentUser.followings.map((friendId) => {
+                return Post.find({ userId: friendId })
+            }));
+        res.json(userPosts.concat(...friendPosts))
+    } catch (err) {
+        res.status(500).json(err)
+    }
+})
 
 module.exports = router;
