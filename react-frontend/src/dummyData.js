62ec08cdbaeef2aba55b042bb831be5e765203df
Abdul Rafey <csyedabdulrafeyali@gmail.com>
2022-02-17 21:47:14 +0500

    Main__React__DynamicData__Part1

---
+++ react-frontend/src/dummyData.js
@@ -0,0 +1,101 @@
+export const Users = [
+    {
+        id: 1,
+        profilePicture: "assets/person/1.jpeg",
+        username: "Safak Kocaoglu"
+    },
+    {
+        id: 2,
+        profilePicture: "assets/person/2.jpeg",
+        username: "Janell Shrum"
+
+    },
+    {
+        id: 3,
+        profilePicture: "assets/person/3.jpeg",
+        username: "Alex Durden"
+
+    },
+    {
+        id: 4,
+        profilePicture: "assets/person/4.jpeg",
+        username: "Janell Shrum"
+    },
+    {
+        id: 5,
+        profilePicture: "assets/person/5.jpeg",
+        username: "Dante"
+    },
+    {
+        id: 6,
+        profilePicture: "assets/person/6.jpeg",
+        username: "Mooroooo"
+    },
+    {
+        id: 7,
+        profilePicture: "assets/person/7.jpeg",
+        username: "Inaki"
+    },
+    {
+        id: 8,
+        profilePicture: "assets/person/8.jpeg",
+        username: "Mitsuki"
+    },
+    {
+        id: 9,
+        profilePicture: "assets/person/9.jpeg",
+        username: "Shikamaru"
+    },
+    {
+        id: 10,
+        profilePicture: "assets/person/10.jpeg",
+        username: "Obito"
+    }
+];
+
+export const Posts = [
+    {
+        id: 1,
+        desc: "Love for all, Hatred for none",
+        photo: "assets/post/1.jpeg",
+        date: "5 mins ago",
+        userId: 1,
+        like: 32,
+        comment: 9
+    },
+    {
+        id: 2,
+        photo: "assets/post/2.jpeg",
+        date: "15 mins ago",
+        userId: 2,
+        like: 2,
+        comment: 1
+    },
+    {
+        id: 3,
+        desc: "Wonderful Thing",
+        photo: "assets/post/3.jpeg",
+        date: "25 mins ago",
+        userId: 3,
+        like: 44,
+        comment: 13
+    },
+    {
+        id: 4,
+        desc: "This Time shall pass too",
+        photo: "assets/post/4.jpeg",
+        date: "2 mins ago",
+        userId: 4,
+        like: 9,
+        comment: 4
+    },
+    {
+        id: 5,
+        desc: "Problems always do exists",
+        photo: "assets/post/5.jpeg",
+        date: "38 mins ago",
+        userId: 5,
+        like: 35,
+        comment: 21
+    }
+]
