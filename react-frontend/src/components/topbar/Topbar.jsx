c363e0ee27d859224d351f86e203b75dae094a86
Abdul Rafey <csyedabdulrafeyali@gmail.com>
2022-03-26 20:41:30 +0500

    Main__Integration__CurrentUserWithContextAPI

--- react-frontend/src/components/topbar/Topbar.jsx
+++ react-frontend/src/components/topbar/Topbar.jsx
@@ -1,11 +1,15 @@
 import "./topbar.css";
 import { Search, Person, Chat, Notifications } from "@material-ui/icons"
 import { Link } from "react-router-dom"
+import { AuthContext } from "../../context/AuthContext"
+import { useContext } from "react";
 function Topbar() {
+    const { user } = useContext(AuthContext);
+    const PF = process.env.REACT_APP_PUBLIC_FOLDER;
     return (
         <div className='topbarContainer'>
             <div className="topbarLeft">
-                <Link to="/" style={{textDecoration: 'none'}}>
+                <Link to="/" style={{ textDecoration: 'none' }}>
                     <span className="logo">GawainBook</span>
                 </Link>
             </div>
@@ -34,7 +38,9 @@ function Topbar() {
                         <span className="topbarIconBadge">1</span>
                     </div>
                 </div>
-                <img src="/assets/person/1.jpeg" alt="" className="topbarImg" />
+                <Link to={`/profile/${user.username}`}>
+                    <img src={user.profilePicture ? PF + user.profilePicture : PF + "person/noAvatar.png"} alt="" className="topbarImg" />
+                </Link>
             </div>
         </div>);
 }
