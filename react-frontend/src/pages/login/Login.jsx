baf995fc1fe106d5fd253a9a78a9dac74dd6e607
Abdul Rafey <csyedabdulrafeyali@gmail.com>
2022-03-26 09:53:17 +0500

    Main__Integration__ContextAPI

--- react-frontend/src/pages/login/Login.jsx
+++ react-frontend/src/pages/login/Login.jsx
@@ -1,27 +1,35 @@
 import "./login.css"
+import { useRef } from 'react';
 
 export default function Login() {
-  return (
-    <div className="login">
-        <div className="loginWrapper">
-            <div className="loginLeft">
-                <h3 className="loginLogo">GawainBook</h3>
-                <span className="loginDesc">
-                    Connect with friends and the world around you on GawainBook.
-                </span>
-            </div>
-            <div className="loginRight">
-                <div className="loginBox">
-                    <input placeholder="Email" className="loginInput" />
-                    <input placeholder="Password" className="loginInput" />
-                    <button className="loginButton">Log In</button>
-                    <span className="loginForgot">Forgot Password?</span>
-                    <button className="loginRegisterButton">
-                        Create a New Account
-                    </button>
+    const email = useRef();
+    const password = useRef();
+
+    const handleClick = (e) => {
+        e.preventDefault();
+        console.log(email.current.value)
+    }
+    return (
+        <div className="login">
+            <div className="loginWrapper">
+                <div className="loginLeft">
+                    <h3 className="loginLogo">GawainBook</h3>
+                    <span className="loginDesc">
+                        Connect with friends and the world around you on GawainBook.
+                    </span>
+                </div>
+                <div className="loginRight">
+                    <form className="loginBox" onSubmit={handleClick}>
+                        <input placeholder="Email" required type="email" className="loginInput" ref={email} />
+                        <input placeholder="Password" required minLength="6" className="loginInput" ref={password} type="password"  />
+                        <button className="loginButton">Log In</button>
+                        <span className="loginForgot">Forgot Password?</span>
+                        <button className="loginRegisterButton">
+                            Create a New Account
+                        </button>
+                    </form>
                 </div>
             </div>
         </div>
-    </div>
-  )
+    )
 }
